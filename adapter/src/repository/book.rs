8ba2644e50bac27bc91b917a8637a0c62732be34
shuntaka9576 <shuntaka9576@gmail.com>
2024-10-08 15:25:46 +0900

    20241008-7

--- adapter/src/repository/book.rs
+++ adapter/src/repository/book.rs
@@ -1,11 +1,14 @@
 use crate::database::{model::book::BookRow, ConnectionPool};
-use anyhow::Result;
 use async_trait::async_trait;
 use derive_new::new;
 use kernel::{
-    model::book::{event::CreateBook, Book},
+    model::{
+        book::{event::CreateBook, Book},
+        id::BookId,
+    },
     repository::book::BookRepository,
 };
+use shared::error::{AppError, AppResult};
 use uuid::Uuid;
 
 #[derive(new)]
@@ -15,7 +18,7 @@ pub struct BookRepositoryImpl {
 
 #[async_trait]
 impl BookRepository for BookRepositoryImpl {
-    async fn create(&self, event: CreateBook) -> Result<()> {
+    async fn create(&self, event: CreateBook) -> AppResult<()> {
         sqlx::query!(
             r#"
                 INSERT INTO books (title, author, isbn, description)
@@ -27,12 +30,13 @@ impl BookRepository for BookRepositoryImpl {
             event.description,
         )
         .execute(self.db.inner_ref())
-        .await?;
+        .await
+        .map_err(AppError::SpecificOperationError)?;
 
         Ok(())
     }
 
-    async fn find_all(&self) -> Result<Vec<Book>> {
+    async fn find_all(&self) -> AppResult<Vec<Book>> {
         let rows: Vec<BookRow> = sqlx::query_as!(
             BookRow,
             r#"
@@ -52,7 +56,7 @@ impl BookRepository for BookRepositoryImpl {
         Ok(rows.into_iter().map(Book::from).collect())
     }
 
-    async fn find_by_id(&self, book_id: Uuid) -> Result<Option<Book>> {
+    async fn find_by_id(&self, book_id: BookId) -> AppResult<Option<Book>> {
         let row: Option<BookRow> = sqlx::query_as!(
             BookRow,
             r#"
@@ -65,7 +69,7 @@ impl BookRepository for BookRepositoryImpl {
            FROM books
            WHERE book_id = $1
             "#,
-            book_id
+            book_id as _ // query_as!マクロによるコンパイル時の型チェックを無効化
         )
         .fetch_optional(self.db.inner_ref())
         .await?;
