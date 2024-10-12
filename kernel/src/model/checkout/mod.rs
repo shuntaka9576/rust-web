0dfad15622f126ed243d644042f4fe4de29da6e6
shuntaka9576 <shuntaka9576@gmail.com>
2024-10-12 14:06:57 +0900

    20241012

---
+++ kernel/src/model/checkout/mod.rs
@@ -0,0 +1,20 @@
+use chrono::{DateTime, Utc};
+
+use super::id::{BookId, CheckoutId, UserId};
+
+pub mod event;
+
+pub struct Checkout {
+    pub id: CheckoutId,
+    pub checkout_out_by: UserId,
+    pub checkout_out_at: DateTime<Utc>,
+    pub returned_at: Option<DateTime<Utc>>,
+    pub book: CheckoutBook,
+}
+
+pub struct CheckoutBook {
+    pub book_id: BookId,
+    pub title: String,
+    pub author: String,
+    pub isbn: String,
+}
