fe62e5d2c617a6eae01e33292bbf42981e9a0e0f
shuntaka9576 <shuntaka9576@gmail.com>
2024-10-13 07:27:32 +0900

    20241013-fix

--- data/initial_setup.sql
+++ data/initial_setup.sql
@@ -16,25 +16,3 @@ FROM
   roles
 WHERE
   name LIKE 'Admin';
-
-CREATE TABLE IF NOT EXISTS checkouts(
-  checkout_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  book_id UUID NOT NULL UNIQUE,
-  user_id UUID NOT NULL,
-  checked_out_at TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-
-  FOREIGN KEY (book_id) REFERENCES books(book_id)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE,
-  FOREIGN KEY (book_id) REFERENCES users(user_id)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS returned_checkouts (
-  checkout_id UUID PRIMARY KEY,
-  book_id UUID NOT NULL,
-  user_id UUID NOT NULL,
-  checked_out_at TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-  returned_at TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(3)
-);
