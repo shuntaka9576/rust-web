5abf82b545b1ebd6ed5af870c00e535e79001b8a
shuntaka9576 <shuntaka9576@gmail.com>
2024-10-13 17:03:15 +0900

    logger今更有効化

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -17,6 +17,7 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilte
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    init_logger()?;
     bootstrap().await
 }
 
@@ -66,7 +67,7 @@ async fn bootstrap() -> Result<()> {
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
     let listener = TcpListener::bind(&addr).await?;
 
-    println!("Listening on {}", addr);
+    tracing::info!("Listening on {}", addr);
 
     axum::serve(listener, app).await.map_err(Error::from)
 }
